67e8d58bf2bec89845402fa20b96d54ac4e00f03
SebastianBuftea <sebibuftea85@gmail.com>
2024-05-21 16:14:17 +0200

    first commit

--- src/exercise/oop3/Book.java
+++ src/exercise/oop3/Book.java
@@ -1,4 +1,60 @@
-package java.oop3;
+package exercise.oop3;
 
 public class Book {
+    private String titolo;
+    private int numPage;
+    private String author;
+    private String editor;
+
+    public Book(String titolo, String editor, String author, int numPage ) {
+        setTitolo(titolo);
+        setNumPage(numPage);
+        setAuthor(author);
+        setEditor(editor);
+    }
+
+    /*setter*/
+    public void setTitolo(String titolo) throws IllegalArgumentException{
+        if (titolo == null || titolo.trim().isEmpty()) {
+            throw new IllegalArgumentException(" Il titolo non puo essere vuoto");
+        }
+
+        this.titolo=titolo;
+    }
+
+    public void setEditor(String editor) throws IllegalArgumentException{
+        if (editor == null || editor.trim().isEmpty()) {
+            throw new IllegalArgumentException(" l' editore non puo essere vuoto");
+        }
+        this.editor=editor;
+    }
+
+    public void setAuthor(String author) throws IllegalArgumentException{
+        if (author == null || author.trim().isEmpty()) {
+            throw new IllegalArgumentException(" l'autore non puo essere vuoto");
+        }
+        this.author=author;
+    }
+
+    public void setNumPage(int numPage )throws IllegalArgumentException{
+        if (numPage <= 0) {
+            throw new IllegalArgumentException("Il numero di pagine deve essere maggiore di zero");
+        }
+        this.numPage=numPage;
+    }
+
+
+    /*getter*/
+    public String getTitolo() {
+        return titolo;
+    }
+    public String getEditor(){
+        return editor;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public int getNumPage(){
+        return numPage;
+    }
 }
--- src/exercise/oop3/Main.java
+++ src/exercise/oop3/Main.java
@@ -1,20 +1,45 @@
-package java.oop3;
+package exercise.oop3;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         int numOfBooks;
-        String titolo;
-        int numPage;
-        String autor;
-        String editor;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Quanti libri vuoi aggiungere? ");
         numOfBooks = scanner.nextInt();
+        scanner.nextLine();
 
-       /* Book[] books=new Book[numOfBooks];*/
+        Book[] books=new Book[numOfBooks];
+
+        for(int i=0; i<numOfBooks; i++){
+            try{
+                System.out.println("Inserisci il libro numero " + (i + 1));
+
+                System.out.print("Titolo: ");
+                String titolo= scanner.nextLine();
+
+                System.out.print("Editore: ");
+                String editor= scanner.nextLine();
+
+                System.out.print("Autore: ");
+                String author= scanner.nextLine();
+
+                System.out.print("Numero di pagine: ");
+                int numPage= scanner.nextInt();
+                scanner.nextLine();
+
+                Book book=new Book(titolo, editor, author, numPage);
+                books[i]=book;
+            }catch (Exception e){
+                System.out.println( "errore durante l'inserimento"+ e.getMessage());
+                System.out.println(" ");
+                i--;
+            }
+
+        }
 
     }
 }
